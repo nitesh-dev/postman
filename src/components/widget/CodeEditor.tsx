523a1862b43cb11eac5b984d8a3046da31ba304d
nitesh-dev <niteshdev547@gmail.com>
2024-06-13 01:35:22 +0530

    editor added

--- src/components/widget/CodeEditor.tsx
+++ src/components/widget/CodeEditor.tsx
@@ -1,12 +1,33 @@
 import { Editor } from "@monaco-editor/react";
+import { useRef } from "react";
 
 export default function CodeEditor() {
+  const monacoRef = useRef(null);
+
+  function handleEditorDidMount(editor: any, monaco: any) {
+    // here is another way to get monaco instance
+    // you can also store it in `useRef` for further usage
+    monacoRef.current = monaco;
+  }
   return (
-    <div className="code-editor" style={{ height: 200 }}>
+    <div className="code-editor" style={{ height: 250 }}>
       <Editor
-        theme="vs-dark"
-        defaultLanguage="javascript"
+        theme="vs-light"
+        defaultLanguage="json"
         defaultValue="// some comment"
+        options={{
+          minimap: { enabled: false },
+          readOnly: false,
+          fontSize: 14,
+          fontFamily: "Menlo, Monaco, 'Courier New', monospace",
+          lineNumbers: "on",
+          wordWrap: "off",
+          scrollBeyondLastLine: false,
+          automaticLayout: true,
+          contextmenu: false,
+          glyphMargin: false,
+        }}
+        onMount={handleEditorDidMount}
       />
     </div>
   );
